4244a3cb9ba5a900b07a356fc0864aec8aa39e81
mhoh <hun1004@gmail.com>
2017-02-16 00:45:58 +0900

    DataGather Daemon 개발시작 DG Listener 추가 컨테이너 기동 시 데몬 시작

---
+++ src/daemon/GatherListener.java
@@ -0,0 +1,23 @@
+package daemon;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class GatherListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent sce) {
+
+        Thread th = new Thread() {
+            public void run() {
+                // implement daemon logic here.
+            }
+        };
+        th.setDaemon(true);
+        th.start();
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+        // you could notify your thread you're shutting down if
+        // you need it to clean up after itself
+    }
+}
